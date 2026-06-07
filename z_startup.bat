e733a1769345005d8dcde09308575b2bcea695d6
InZee <187462853+inzeets@users.noreply.github.com>
2026-06-06 19:56:55 -0700

    speedup startup

--- z_startup.bat
+++ z_startup.bat
@@ -1,7 +1,10 @@
 set "LINK=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\%~n0.lnk"
-if not exist "%LINK%" powershell -NoProfile -Command "$s=(New-Object -ComObject WScript.Shell).CreateShortcut('%LINK%');$s.TargetPath='%~f0';$s.WorkingDirectory='%~dp0';$s.Save()"
+if not exist "%LINK%" (
+    powershell -NoProfile -Command "$s=(New-Object -ComObject WScript.Shell).CreateShortcut('%LINK%');$s.TargetPath='%~f0';$s.WorkingDirectory='%~dp0';$s.Save()"
+    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Serialize" /v StartupDelayInMSec /t REG_DWORD /d 0 /f
+)
 
 cd %~dp0
 start AutoHotkey64.exe keys.ahk
-start AutoHotkey64.exe StickyScreenshots.ahk
+::start AutoHotkey64.exe StickyScreenshots.ahk
 
